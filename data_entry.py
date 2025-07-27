b84070181c8cff80ce5f9bc87da18f56f32dc6d6
shabnam-codes <shabnam0613@gmail.com>
2025-07-27 21:14:13 +0530

    data_entry.py

--- data_entry.py
+++ data_entry.py
@@ -1,5 +1,5 @@
 from datetime import datetime 
-
+import pandas as pd
 categories = {"I": "Income", "E": "Expense"}
 
 def get_date(prompt, allow_default=False):
@@ -34,3 +34,37 @@ def get_category():
 
 def get_description():
     return input("Enter a description (optional): ")
+
+
+def get_transactions(start_date, end_date):
+    CSV_FILE = "finance_data.csv"
+    FORMAT = "%d-%m-%Y"
+
+    df = pd.read_csv(CSV_FILE)
+    df["date"] = pd.to_datetime(df["date"], format=FORMAT)
+    start = datetime.strptime(start_date, FORMAT)
+    end = datetime.strptime(end_date, FORMAT)
+
+    mask = (df["date"] >= start) & (df["date"] <= end)
+    filtered_df = df.loc[mask]
+
+    if filtered_df.empty:
+        print("No transactions found in the given date range")
+    else:
+        print(f"Transactions from {start.strftime(FORMAT)} to {end.strftime(FORMAT)}")
+        print(
+            filtered_df.to_string(
+                index=False, formatters={"date": lambda x: x.strftime(FORMAT)}
+            )
+        )
+
+    total_income = filtered_df[filtered_df["category"] == "Income"]["amount"].sum()
+    total_expense = filtered_df[filtered_df["category"] == "Expense"]["amount"].sum()
+
+    print("\nSUMMARY:")
+    print(f"Total Income: ${total_income:.2f}")
+    print(f"Total Expense: ${total_expense:.2f}")
+    print(f"Net savings: ${(total_income - total_expense):.2f}")
+
+    return filtered_df
+
